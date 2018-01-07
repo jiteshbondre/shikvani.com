b79bd2d7be9a190a14a78d5ac448644e20048515
lalitdiwate <lalitdiwate@gmail.com>
2018-01-07 12:26:51 +0530

    By Lalit

--- shikavni/resources/views/welcome.blade.php
+++ shikavni/resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel33
                 </div>
 
                 <div class="links">
